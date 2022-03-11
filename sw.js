01c65cc37d3252eb98d67df3f3a66fd39607cfb5
IrvinCruz22 <183110177@gmail.com>
2022-03-11 15:10:51 -0600

    first commit

--- sw.js
+++ sw.js
@@ -10,3 +10,13 @@ self.addEventListener('fetch' , event => {
     //console.log("Hola desde la intalación del Service Worker UTVAM");
     caches.open( "Espacio 1" );
   });
+  
+
+  self.addEventListener('fetch' , event => {
+   //event.respondWith( fetch(event.request) );  
+ 
+   if( event.request.url.includes('http://localhost:8080/images/')){
+     console.log(event.request.url);
+   }  
+
+ });
