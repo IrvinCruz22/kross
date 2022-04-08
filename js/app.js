daa2ce8bde8af74e8e014b29fdae51a8d7b7547e
IrvinCruz22 <98175751+IrvinCruz22@users.noreply.github.com>
2022-04-08 13:43:37 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //confirmar si soporta sw
 if(navigator.serviceWorker){
     //alert("Soporta el SW")
-    navigator.serviceWorker.register("sw.js");
+    navigator.serviceWorker.register("/kross/sw.js");
 }
 else{
     alert(":( no soporta el SW");
